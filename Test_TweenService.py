4da9af48692a640669cdb255fc195e962485a7e4
flamfrosticboio <jairusheredia@gmail.com>
2024-05-22 13:37:17 +0800

    modified it to be testable

--- Test_TweenService.py
+++ Test_TweenService.py
@@ -1,23 +1,40 @@
-import TweenService
-from TweenService.Easings import EaseInExpo
-
-TweenService.init()
-
-t = TweenService.TweenHandler(1, {}, {})
-print(t.start())
-
 import pygame as pg 
+import TweenService
+
+tween_thread_main = TweenService.create_main_thread()
+
+class Frame:
+    def __init__(self, pos, size, color):
+        self.image = pg.Surface(size)
+        self.rect = pg.Rect(pos, size)
+        self.image.fill(color)
+
+    def render(self, surface):
+        surface.blit(self.image, self.rect)
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = (680, 420)
 screen = pg.display.set_mode(SCREEN_SIZE)
 clock = pg.time.Clock() 
 
-running = True
+allframes = [
+    Frame((10, 10), (50, 50), (0, 255, 0))
+]
+
+item_a = allframes[0].rect
+tween_a = TweenService.TweenHandler(item_a, TweenService.TweenInfo(), {"left": 100})
+tween_a.start()
+
+tween_a.Completed.Connect(lambda *args, **kwargs: allframes[0].image.fill((0, 255, 0)))
+
+running = True 
 while running:
     for e in pg.event.get():
         if e.type == pg.QUIT: running = False 
+    tween_thread_main.update()
+    screen.fill((255, 255, 255))
 
-    screen.fill((255, 255, 255)) 
+    for frame in allframes:
+        frame.render(screen)
 
     pg.display.flip()
     clock.tick(60)
